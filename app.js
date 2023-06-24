22c55a4d64a2e20a176db79c7f1aaa74735d3509
kowais915 <kowais915@gmail.com>
2023-06-25 01:14:59 +0500

    :alembic: Added a test endpoint

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ const server = app.listen(3000, ()=>{
     console.log("Listening to request at port 3000.")
 })
 
+app.get('/cool', (req, res)=>{
+    res.status(200).json({
+        msg: "wow"
+    })
+})
 
 const io = socket(server);
 
