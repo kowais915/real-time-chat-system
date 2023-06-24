b5f13f56fa59c5d3ed88fd4106f9f638aaf92e39
kowais915 <kowais915@gmail.com>
2023-06-24 21:05:04 +0500

    :construction: Setup the first endpoint

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const express = require('express');
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+
+app.listen(3000, ()=>{
+    console.log("Listening to request at port 3000.")
+})
+
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        msg: "hello, socket!"
+    })
+})
