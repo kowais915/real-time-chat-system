c6a6c6001b926c74f6ce4129e1f0fb589751ad1e
kowais915 <kowais915@gmail.com>
2023-06-25 01:35:01 +0500

    :construction: Working on deployment to railway

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
 
 
 
@@ -20,10 +21,8 @@ const server = app.listen(3000, ()=>{
     console.log("Listening to request at port 3000.")
 })
 
-app.get('/cool', (req, res)=>{
-    res.status(200).json({
-        msg: "wow"
-    })
+app.get('/', (req, res)=>{
+    res.sendFile('./public/index.html', {root: __dirname})
 })
 
 const io = socket(server);
