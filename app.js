5746043cd0bb7802130c33ab3e580f2d2131a4a3
kowais915 <kowais915@gmail.com>
2023-06-24 21:06:45 +0500

    :construction: Added index.html in the public folder

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 
 
+
 // middleware
 app.use(express.json());
+app.use(express.static('public'));
+
 
 app.listen(3000, ()=>{
     console.log("Listening to request at port 3000.")
@@ -16,3 +19,4 @@ app.get('/', (req, res)=>{
         msg: "hello, socket!"
     })
 })
+
